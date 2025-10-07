fdb94314db7899aae7fe79f5e84ca25ff13cf1e3
AartiW-12 <aartiwhaval12@gmail.com>
2025-10-07 15:46:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your JS code here. If required.
+const progress = document.getElementById("progress-line");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle")
+
+let current = 1;
+
+next.addEventListener("click", ()=>{
+	current++;
+	if(current > circles.length){
+		current = circles.length;
+	}
+	update()
+})
+
+prev.addEventListener("click",()=>{
+	current--;
+	if(current<1){
+		current=1;
+	}
+	update();
+})
+
+function update() {
+	circles.forEach((circle,index) => {
+		if(index < current){
+			circle.classList.add("active")
+		}
+		else{
+			circle.classList.remove("active")
+		}
+	})
+
+	progress.style.width = ((current - 1)/(circles.length - 1))*100 + "%";
+
+	if(current === 1){
+		prev.disabled= true;
+	}
+	else{
+		prev.disabled= false
+	}
+	if(current === circles.length){
+		next.disabled = true;
+	}
+	else{
+		prev.disabled = false;
+	}
+}
